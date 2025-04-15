5b30f90c6aac7706b51127c6cf6e0141945c1682
Rohan1107 <rohankgaikwad@gmail.com>
2025-04-15 00:57:57 -0700

    initial HTML, CSS and JavaScript setup

--- to_do.js
+++ to_do.js
@@ -1,7 +1,42 @@
 
 let x;
+let y;
+let t_id;
+let parentElement;
+let parentElementOne;
+let parentElementTwo;
+let childElement;
+let childElementOne;
+let childElementTwo;
+let newElement;
+let newElementOne;
+let newElementTwo;
+
+let arr = [];
+
+
 
 function addTask(){
+
     x = document.getElementById("add-task-field").value;
-    document.getElementById("output-field").innerHTML = x;
+    arr.push(x);
+    y = document.getElementById("add-task-field").value = "";
+
+    parentElement = document.getElementById("checkbox-div");
+    newElement = document.createElement("input");
+    newElement.setAttribute("type", "checkbox");
+    newElement.setAttribute("id", "task-done");
+    parentElement.appendChild(newElement);
+
+
+    for(let i=0;i<arr.length;i++){
+        parentElementOne = document.getElementById("task-name-div");
+        newElementOne = document.createElement("div");
+        newElementOne.setAttribute("id", i);
+        parentElementOne.appendChild(newElementOne);
+
+        document.getElementById(i).innerHTML = arr[i];
+
+    }
+
 }
